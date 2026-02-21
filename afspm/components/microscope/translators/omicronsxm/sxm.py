ed9503dea2fa23b482c026e5c10a9397d865dff0
Nick Sullivan <nsulmol@gmail.com>
2026-02-21 17:07:42 -0500

    [fix] minor logic fixes
    - Needed to convert a str to an int.
    - In _evaluate_response(), we extract the method both if our indeces
    match OR if its our first command.

--- afspm/components/microscope/translators/omicronsxm/sxm.py
+++ afspm/components/microscope/translators/omicronsxm/sxm.py
@@ -459,7 +459,7 @@ class DDEClient(object):
             logger.trace('Received response with only header. Skipping.')
             return
 
-        received_index = strs[0].split(' ')[1]  # Get # from header
+        received_index = int(strs[0].split(' ')[1])  # Get # from header
         message = strs[1]
 
         first_command = self._command_index is None
@@ -469,7 +469,7 @@ class DDEClient(object):
 
         if first_command:  # Initialize our counter
             self._command_index = received_index
-        if indices_match:
+        if first_command or indices_match:
             if message == '':
                 return None
             if ERROR_PREFIX in message:
