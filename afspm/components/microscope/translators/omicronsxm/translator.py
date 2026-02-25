ba7084fc80bf2d3b4018f93f8a1ac486a737a763
Nick Sullivan <nsulmol@gmail.com>
2026-02-25 17:53:27 -0500

    [fix] undo mistaken changes commit float tolerance
    You mistakenly pushed undesired changes. Undoing them here.

--- afspm/components/microscope/translators/omicronsxm/translator.py
+++ afspm/components/microscope/translators/omicronsxm/translator.py
@@ -261,12 +261,9 @@ class SXMTranslator(ct.ConfigTranslator):
 
         With this override, we meet our black-box expectations.
         """
-        # TODO: Issue, this cannot get called to store the probe pos
-        # in on_action_request() (because we are alread in SS_SCANNING)?
         if self.scope_state in [scan_pb2.ScopeState.SS_FREE,
                                 scan_pb2.ScopeState.SS_UNDEFINED]:
-            self._latest_probe_pos = super().poll_probe_pos()
-            return self._latest_probe_pos
+            return super().poll_probe_pos()
         return self.probe_pos
 
     def _handle_polling_device(self):
