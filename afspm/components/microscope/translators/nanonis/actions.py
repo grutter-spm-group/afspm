ffe2a7d750d2696c20b60092c53071568522d158
Nick Sullivan <nsulmol@gmail.com>
2026-02-27 16:38:00 -0500

    [fix] nanonis workaround spec start synchronous
    It turns out the spec-start calls are all synchronous, blocking any
    other calls we might do on that port until it ends. This is regardless
    of whether we request a response or not.
    
    A neat workaround: we just maintain 2 connections: one port for most of
    our communications, and one port restricted just to spec-start calls!
    That's what we're doing here.

--- afspm/components/microscope/translators/nanonis/actions.py
+++ afspm/components/microscope/translators/nanonis/actions.py
@@ -14,16 +14,27 @@ logger = logging.getLogger(__name__)
 class NanonisActionHandler(actions.ActionHandler):
     """Implements Nanonis-specific action handling.
 
+    Note: it turns out the spec start command is always synchronous. We can
+    work around this limitation by having a separate client connection just
+    for spec start calls.
+
     Attributes:
         _client: TCP client used to communicate with Nanonis.
+        _spec_start_client: TCP client used to send spec start calls,
+            since it is synchronous and thus breaks other polls.
         _scan_direction: holds the latest direction we will use when
             sending the request out.
         _spectroscopy_mode: the mode of spectroscopy we use for our specs.
     """
 
-    def __init__(self, client: clnt.NanonisClient, **kwargs):
+    def __init__(self, client: clnt.NanonisClient,
+                 spec_start_port: int = clnt.DEFAULT_SPEC_START_PORT,
+                 **kwargs):
         """Set up spectroscopy mode."""
         self._client = client
+        self._spec_start_client = clnt.NanonisClient(
+            client._host, spec_start_port, client._timeout_s,
+            client._bufsize)
         self._scan_direction = 0
         self._spectroscopy_mode = spectroscopy.SpectroscopyMode.BIAS
         super().__init__(**kwargs)
@@ -52,6 +63,7 @@ def spec_action(handler: NanonisActionHandler,
     action = scan.ScanAction(action)  # Validate the number is valid
 
     if action == scan.ScanAction.START:
+        client = handler._spec_start_client
         if handler._spectroscopy_mode == spectroscopy.SpectroscopyMode.Z:
             req = spectroscopy.ZSpectraStartReq()
             rep = spectroscopy.ZSpectraStartRep()
@@ -59,6 +71,7 @@ def spec_action(handler: NanonisActionHandler,
             req = spectroscopy.BiasSpectraStartReq()
             rep = spectroscopy.BiasSpectraStartRep()
     else:
+        client = handler._client
         if handler._spectroscopy_mode == spectroscopy.SpectroscopyMode.Z:
             req = spectroscopy.ZSpectraStopReq()
             rep = spectroscopy.ZSpectraStopRep()
@@ -66,8 +79,7 @@ def spec_action(handler: NanonisActionHandler,
             req = spectroscopy.BiasSpectraStopReq()
             rep = spectroscopy.BiasSpectraStopRep()
 
-    send_request(handler._client, req, rep if req.request_response()
-                 else None)
+    send_request(client, req, rep if req.request_response() else None)
 
 
 def send_request(client: clnt.NanonisClient, req: base.NanonisRequest,
--- afspm/components/microscope/translators/nanonis/client.py
+++ afspm/components/microscope/translators/nanonis/client.py
@@ -12,6 +12,7 @@ logger = logging.getLogger(__name__)
 # TODO: Review these defaults!
 DEFAULT_HOST = '127.0.0.1'
 DEFAULT_PORT = 6501
+DEFAULT_SPEC_START_PORT = 6502
 DEFAULT_TIMEOUT_S = 0.25
 DEFAULT_BUFSIZE = 1024
 
