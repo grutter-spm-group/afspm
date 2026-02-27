7e7085ae6ebe1dbb25a6aa83b0c8a07e8f994bc2
Nick Sullivan <nsulmol@gmail.com>
2026-02-27 14:41:17 -0500

    [refactor] nanonis make ActionHandler cleaner
    We had this complicated guid_to_reqrep_map when we could just make the
    methods a bit more complicated.

--- afspm/components/microscope/translators/nanonis/actions.py
+++ afspm/components/microscope/translators/nanonis/actions.py
@@ -16,78 +16,58 @@ class NanonisActionHandler(actions.ActionHandler):
 
     Attributes:
         _client: TCP client used to communicate with Nanonis.
-        _guid_to_reqrep_map: mapping from general action ID to
-            request-reply structures.
+        _scan_direction: holds the latest direction we will use when
+            sending the request out.
+        _spectroscopy_mode: the mode of spectroscopy we use for our specs.
     """
 
     def __init__(self, client: clnt.NanonisClient, **kwargs):
         """Set up spectroscopy mode."""
         self._client = client
-        self._guid_to_reqrep_map = {}
-        self._populate_guid_to_reqrep_map()
+        self._scan_direction = 0
+        self._spectroscopy_mode = spectroscopy.SpectroscopyMode.BIAS
         super().__init__(**kwargs)
-
-    def _populate_guid_to_reqrep_map(self):
-        self._guid_to_reqrep_map[actions.MicroscopeAction.START_SCAN] = (
-            base.NanonisReqRep(scan.ScanActionReq(action=scan.ScanAction.START),
-                               scan.ScanActionRep()))
-        self._guid_to_reqrep_map[actions.MicroscopeAction.STOP_SCAN] = (
-            base.NanonisReqRep(scan.ScanActionReq(action=scan.ScanAction.STOP),
-                               scan.ScanActionRep()))
-        # Set default spectroscopy mapping
-        self._update_spec_mappings(spectroscopy.SpectroscopyMode.BIAS)
-
-    def _update_spec_mappings(self, mode: spectroscopy.SpectroscopyMode):
-        """Update mappings for spectroscopy actions based on current mode."""
-        if mode == spectroscopy.SpectroscopyMode.BIAS:
-            start_req = spectroscopy.BiasSpectraStartReq()
-            start_rep = spectroscopy.BiasSpectraStartRep()
-            stop_req = spectroscopy.BiasSpectraStopReq()
-            stop_rep = spectroscopy.BiasSpectraStopRep()
-        else:
-            start_req = spectroscopy.ZSpectraStartReq()
-            start_rep = spectroscopy.ZSpectraStartRep()
-            stop_req = spectroscopy.ZSpectraStopReq()
-            stop_rep = spectroscopy.ZSpectraStopRep()
-
-        self._guid_to_reqrep_map[actions.MicroscopeAction.START_SPEC] = (
-            base.NanonisReqRep(start_req, start_rep))
-        self._guid_to_reqrep_map[actions.MicroscopeAction.STOP_SPEC] = (
-            base.NanonisReqRep(stop_req, stop_rep))
 
     def set_spectroscopy_mode(self, mode: spectroscopy.SpectroscopyMode):
         """Switch to using the appropriate spectroscopy mode."""
-        self._update_spec_mappings(mode)
-
-    def _call_action(self, guid: str) -> base.NanonisResponse | None:
-        """Mimics _get_param_spm_struct method in params.py."""
-        try:
-            req_rep = self._guid_to_reqrep_map[guid]
-        except KeyError:
-            msg = f'Could not find NanonisMessages for {guid}.'
-            raise actions.ActionConfigurationError(msg)
-
-        send_request(self._client, req_rep.req,
-                     req_rep.rep if req_rep.req.request_response()
-                     else None)
+        self._spectroscopy_mode = mode
 
 
 def scan_action(handler: NanonisActionHandler,
-                action: scan.ScanAction):
+                action: int):
     """Start/stop scan."""
-    guid = (actions.MicroscopeAction.START_SCAN
-            if action == scan.ScanAction.START
-            else actions.MicroscopeAction.STOP_SCAN)
-    handler._call_action(guid)
+    action = scan.ScanAction(action)  # Validate the number is valid
+    req = scan.ScanActionReq(action=action.value,  # Convert to int again
+                             direction=handler._scan_direction % 2)
+    handler._scan_direction += 1  # Increment to change direction
+    rep = scan.ScanActionRep()
+
+    send_request(handler._client, req, rep if req.request_response()
+                 else None)
 
 
 def spec_action(handler: NanonisActionHandler,
-                action: scan.ScanAction):
+                action: int):
     """Start/stop spec."""
-    guid = (actions.MicroscopeAction.START_SPEC
-            if action == scan.ScanAction.START
-            else actions.MicroscopeAction.STOP_SPEC)
-    handler._call_action(guid)
+    action = scan.ScanAction(action)  # Validate the number is valid
+
+    if action == scan.ScanAction.START:
+        if handler._spectroscopy_mode == spectroscopy.SpectroscopyMode.Z:
+            req = spectroscopy.ZSpectraStartReq()
+            rep = spectroscopy.ZSpectraStartRep()
+        else:
+            req = spectroscopy.BiasSpectraStartReq()
+            rep = spectroscopy.BiasSpectraStartRep()
+    else:
+        if handler._spectroscopy_mode == spectroscopy.SpectroscopyMode.Z:
+            req = spectroscopy.ZSpectraStopReq()
+            rep = spectroscopy.ZSpectraStopRep()
+        else:
+            req = spectroscopy.BiasSpectraStopReq()
+            rep = spectroscopy.BiasSpectraStopRep()
+
+    send_request(handler._client, req, rep if req.request_response()
+                 else None)
 
 
 def send_request(client: clnt.NanonisClient, req: base.NanonisRequest,
